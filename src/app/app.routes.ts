ed2ecd1146be3965a8f5e700569ed3b8f7f808c9
aissatouasteldione <aissatou.dione@ism.edu.sn>
2025-06-01 00:20:28 +0000

    liste etudiants actifs de l'année

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { authGuard } from './guards/auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AbsencesComponent } from './pages/absences/absences.component';
 import { AbsenceDetailComponent } from './pages/absences/absence-detail/absence-detail.component';
+import { EtudiantsComponent } from './pages/etudiants/etudiants.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -15,6 +16,7 @@ export const routes: Routes = [
       { path: 'absences', component: AbsencesComponent },
       { path: 'absences/:id', component: AbsenceDetailComponent },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'etudiants', component: EtudiantsComponent },
     ],
   },
   { path: '**', redirectTo: 'login' },
